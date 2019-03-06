aba265db149dc328d2b3af5376c242bfe3beb896
Chinmay <cmhebbal@gmail.com>
2019-03-07 00:39:48 +0530

    botstrap stuff

--- myblog/app/models.py
+++ myblog/app/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     title= models.CharField(max_length=100)
     slug=models.SlugField(max_length=120)
     author=models.ForeignKey(User ,related_name='blog_posts',on_delete=False)
+    body=models.TextField(default="This is the body this is to check")
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
     status= models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
